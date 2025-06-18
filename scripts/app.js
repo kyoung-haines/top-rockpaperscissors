0d2a056e396868e4f2b60514d9c78b2e3b963177
kyoung-haines <k.younghaines@gmail.com>
2025-06-18 09:10:38 -0400

    Added getComputerChoice() function Added a function to app.js that will simulate the computer's choice while playing the game

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,15 @@
-console.log("Hello, World!");
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random()* 3 + 1);
+
+    switch(randomNumber) {
+        case 1:
+            return 'rock';
+            break;
+        case 2:
+            return 'paper';
+            break;
+        case 3:
+            return 'scissors';
+            break;
+    }
+}
