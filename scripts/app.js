38cb145c775758b5a55831991c9509f80345a657
kyoung-haines <k.younghaines@gmail.com>
2025-06-18 09:31:54 -0400

    Added getHumanChoice() function Added the function that will obtain the user's selection

--- scripts/app.js
+++ scripts/app.js
@@ -13,3 +13,19 @@ function getComputerChoice() {
             break;
     }
 }
+
+function getHumanChoice() {
+    let humanChoice = parseInt(prompt('Enter a number between 1-3 (inclusive):'));
+
+    switch(humanChoice) {
+        case 1:
+            return 'rock';
+            break;
+        case 2:
+            return 'paper';
+            break;
+        case 3:
+            return 'scissors';
+            break;
+    }
+}
